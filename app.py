facbff8ae8e83587d2b63d3ce9b82556cd75bf4e
ruthvik1665 <91338309+ruthvik1665@users.noreply.github.com>
2023-06-18 15:27:34 +0000

    added dynamic nature of wesite and added api route

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
-
+JOBS=[{
+  "id":1,
+  "title":'Data Analyst',
+  "location":'Bengaluru',
+  'salary': 1500000
+},
+     {
+  "id":2,
+  "title":'Web Developer',
+  "location":'Bengaluru',
+  'salary': 1500000
+  
+},
+     {
+  "id":3,
+  "title":'Data scientist',
+  "location":'Bengaluru',
+  'salary': 1500000
+},
+     {
+  "id":4,
+  "title":'AI Engineer',
+  "location":'Bengaluru'
+}]
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
-
+  return render_template("home.html",jobs=JOBS)
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
