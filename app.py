10055dc2f8238a4fc797caf1d37f3834a0daa531
ruthvik1665 <91338309+ruthvik1665@users.noreply.github.com>
2023-07-03 14:13:48 +0000

    dynamic job for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template,jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db,load_job_from_db
 
 app = Flask(__name__)
 
@@ -12,6 +12,14 @@ def hello_world():
 def list_jobs():
   jobs=load_jobs_from_db()
   return jsonify(jobs)
+
+@app.route('/job/<id>')
+def show_job(id):
+  job=load_job_from_db(id)
+  if job is None:
+    return "page not found"
+  else:
+    return render_template("jobpage.html",job=job)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',port=4444, debug=False)
--- database.py
+++ database.py
@@ -1,15 +1,28 @@
-from sqlalchemy import create_engine,text
+from sqlalchemy import create_engine, text
 import os
-db_connection_string=os.environ['db_connection']
-engine=create_engine(db_connection_string,connect_args={"ssl":{"ssl_ca": "/etc/ssl/cert.pem"}})
+
+my_secret = os.environ['db_connection']
+engine = create_engine(my_secret,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
 
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
-    jobs=[]
+    jobs = []
     for row in result.all():
       jobs.append(row._asdict())
     return jobs
-    
-    
 
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs where id=:val"), {"val":id})
+    rows = result.all()
+    if len(rows)==0:
+      return None
+    else:
+      return (rows[0]._asdict())
+    
